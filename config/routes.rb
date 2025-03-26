49e3f901ee71e6eb2637a721a3526690fe37d427
manto0530 <brainmercy@gmail.com>
2025-03-26 20:21:46 +0530

    new pagaes dashboard to home and about pages check routes and views

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-  # get "dashboard/index"
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  get 'home', to: "pages#home"
+  # this line tells you if we type home it take us to pages home
+  get 'about', to:  "pages#about"
+  # this line tells you if we type home it take us to pages about - where about is the word we use in creating controller 
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
@@ -11,5 +13,6 @@ Rails.application.routes.draw do
   # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
 
   # Defines the root path route ("/")
-  root "dashboard#index"
+  root "pages#home"
+  # root "pages#home" says it will take to pages home when you load localhost:3000
 end
